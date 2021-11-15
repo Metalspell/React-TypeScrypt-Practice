a76e1b25c69f56fc5f9b8567119829ade64dbd20
metalspell <lonelyrockclimber@gmail.com>
2021-11-15 20:30:48 +0200

    Add storybook and some tests

--- simple-tabmenu/src/App.test.tsx
+++ simple-tabmenu/src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe("App", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  it('headitems render fine', () => {
+    expect(screen.getAllByRole('tab')).toHaveLength(3);
+  })
+})
