9c25855a59b763f8c921a827c5e20f2b47f00604
metalspell <lonelyrockclimber@gmail.com>
2022-12-08 16:56:16 +0200

    Fix backround

--- my-portfolio/pages/_app.tsx
+++ my-portfolio/pages/_app.tsx
@@ -7,17 +7,11 @@ import { ThemeProvider } from 'next-themes';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
-      <div className='grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48'>
-
-        <div className="top-0 bottom-0 left-0 right-0 hidden w-full lg:block h-150 lg:absolute dark:bg-stars dark:bg-repeat dark:bg-black"></div>
-
-        <img className="hidden w-4/12 lg:absolute lg:dark:block h-4/12 right-4" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="img" />
-
-        <div className="top-0 bottom-0 left-0 right-0 hidden bg-transparent bg-repeat h-150 lg:absolute bg-3000 animate-cloudsMove bg-light-clouds w-10000 dark:animate-cloudsMove dark:bg-1000 dark:bg-clouds "></div>
-        <div className='z-10 h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark '>
+      <div className='grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-10 xl:px-48'>
+        <div className='z-10 h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-4 xl:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark '>
           <SideBar />
         </div>
-        <div className="z-10 flex flex-col col-span-12 overflow-hidden bg-white shadow-custom-light dark:shadow-custom-dark rounded-2xl lg:col-span-9 dark:bg-dark-500">
+        <div className="z-10 flex flex-col col-span-12 overflow-hidden bg-white shadow-custom-light dark:shadow-custom-dark rounded-2xl lg:col-span-8 xl:col-span-9 dark:bg-dark-500">
           <NavBar />
           <Component {...pageProps} />
         </div>
--- my-portfolio/pages/_document.tsx
+++ my-portfolio/pages/_document.tsx
@@ -16,7 +16,10 @@ class MyDocument extends Document {
           </link>
           <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         </Head>
-        <body className="overflow-x-hidden bg-fixed bg-400 bg-clear-sky dark:bg-dark-theme dark:text-white">
+        <body className="overflow-x-hidden bg-fixed bg-contain bg-clear-sky dark:bg-dark-theme dark:text-white">
+          <div className="fixed top-0 bottom-0 left-0 right-0 w-full h-full bg-repeat bg-400 bg-light-theme dark:bg-contain dark:bg-stars dark:bg-repeat dark:bg-black"></div>
+          <img className="hidden w-4/12 lg:absolute lg:dark:block h-4/12 right-4" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="img" />
+          <div className="fixed top-0 bottom-0 left-0 right-0 bg-transparent bg-repeat h-150 bg-3000 animate-cloudsMove bg-light-clouds w-10000 dark:animate-cloudsMove dark:bg-1000 dark:bg-clouds "></div>
           <Main />
           <NextScript />
         </body>
